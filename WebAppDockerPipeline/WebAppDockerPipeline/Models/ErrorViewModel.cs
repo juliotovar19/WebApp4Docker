7fdff724449d2c3f56da4863e0ef20cd0e0e62cf
julio tovar <julio.tovar19@hotmail.com>
2022-08-19 09:14:36 -0500

    Code added

---
+++ WebAppDockerPipeline/WebAppDockerPipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppDockerPipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
